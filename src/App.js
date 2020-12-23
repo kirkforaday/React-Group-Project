afbd8ca33384ef0d4d852f110874e07010f94dab
kirkforaday <wrckirk@gmail.com>
2020-12-23 14:00:12 -0600

    Created Navbar and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../src/Components/Navbar';
+import {Route, Switch} from 'react-router-dom';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Footer />
+    </>
   );
 }
 
